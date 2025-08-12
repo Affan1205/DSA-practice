4e4d9082eb1180912241f00bd4703a5bc37a8695
Shaik Affan Ahemad <shaikaffanahemad@gmail.com>
2025-08-12 19:55:38 +0530

    progess update.

--- 4.Functions/fpq6.java
+++ 4.Functions/fpq6.java
@@ -1,6 +1,15 @@
-//
+//To calculate Fibonacci Series up to n numbers.
 public class fpq6 {
     public static void main(String[] args) {
-        
+        int a = 0;
+        int b = 1;
+        System.out.print(a + " " + b + " ");
+        int n = 10;
+        for (int i = 2; i < n; i++) {
+            int sum = a + b;
+            System.out.print(sum + " ");
+            a = b;
+            b = sum;
+        }
     }
 }
