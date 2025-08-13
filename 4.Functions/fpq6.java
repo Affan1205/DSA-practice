26345950ade41998945008c6e79a20935239ebe9
Shaik Affan Ahemad <shaikaffanahemad@gmail.com>
2025-08-13 21:01:16 +0530

    some progress

--- 4.Functions/fpq6.java
+++ 4.Functions/fpq6.java
@@ -1,15 +1,50 @@
-//To calculate Fibonacci Series up to n numbers.
+//Input currency in rupees and output in USD
+
+import java.util.Scanner;
+
 public class fpq6 {
     public static void main(String[] args) {
-        int a = 0;
-        int b = 1;
-        System.out.print(a + " " + b + " ");
-        int n = 10;
-        for (int i = 2; i < n; i++) {
-            int sum = a + b;
-            System.out.print(sum + " ");
-            a = b;
-            b = sum;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the amount : ");
+        double indRupee = input.nextDouble();
+
+        double amount = currencyConverter(indRupee);
+        System.out.print(amount);
+        input.close();
+    }
+
+    static double currencyConverter(double rupees) {
+        Scanner in = new Scanner(System.in);
+
+        double calculate;
+        System.out.print("Which country currency you want: ");
+        String str = in.next();
+        if (str == "doller" || str == "Doller") {
+            calculate = rupees / 85;
+            return calculate;
+
+        } else if (str == "euro" || str == "Euro") {
+            calculate = rupees / 102;
+            return calculate;
+        } else {
+            return 0;
         }
+
     }
 }
+
+// //To calculate Fibonacci Series up to n numbers.
+// public class fpq6 {
+// public static void main(String[] args) {
+// int a = 0;
+// int b = 1;
+// System.out.print(a + " " + b + " ");
+// int n = 10;
+// for (int i = 2; i < n; i++) {
+// int sum = a + b;
+// System.out.print(sum + " ");
+// a = b;
+// b = sum;
+// }
+// }
+// }
